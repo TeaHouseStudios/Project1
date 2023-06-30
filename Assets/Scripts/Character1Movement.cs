7d21de81405207de95225b198a0d7381359e9fa4
Grant <grant.macmillan94@gmail.com>
2023-06-30 16:56:09 -0400

    Actions Map working for character 1

--- Assets/Scripts/Character1Movement.cs
+++ Assets/Scripts/Character1Movement.cs
@@ -6,10 +6,17 @@ using UnityEngine.InputSystem;
 public class Character1Movement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private PlayerInput playerInput;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        PlayerInputActions playerInputActions = new PlayerInputActions();
+        playerInputActions.Character1.Enable();
+        playerInputActions.Character1.Jump.performed += Jump;
+        
     }
 
     // Update is called once per frame
@@ -20,6 +27,7 @@ public class Character1Movement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
+        Debug.Log(context);
         if (context.performed)
         {
             Debug.Log("JUMP!" + context.phase);
