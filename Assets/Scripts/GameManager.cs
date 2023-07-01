67049166adcfa14262ab2478d2a2f8bbaba57342
Grant <grant.macmillan94@gmail.com>
2023-06-30 21:24:47 -0400

    respawn points saved in game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
     private GameObject character1;
     private GameObject character2;
 
     private Character1 character1MoveScript;
     private Character2 character2MoveScript;
 
+    private Transform c1RespawnPoint;
+    private Transform c2RespawnPoint;
+
     public int currentCharacter;
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +39,9 @@ public class GameManager : MonoBehaviour
 
         character1MoveScript = character1.GetComponent<Character1>();
         character2MoveScript = character2.GetComponent<Character2>();
+
+        c1RespawnPoint = character1.transform;
+        c2RespawnPoint = character2.transform;
 
         character2MoveScript.enabled = false;
         currentCharacter = 1;
@@ -44,6 +66,18 @@ public class GameManager : MonoBehaviour
                 currentCharacter = 1;
             }
 
+        }
+    }
+
+    public void RespawnCharacter(GameObject character)
+    {
+        if (character.CompareTag("Character1"))
+        {
+            character1.transform.position = c1RespawnPoint.transform.position;
+        }
+        if (character.CompareTag("Character2"))
+        {
+            character2.transform.position = c2RespawnPoint.transform.position;
         }
     }
 }
