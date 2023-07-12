5b3040bde72d6701d4a38fd8515528de95c28e6a
Grant <grant.macmillan94@gmail.com>
2023-07-12 13:15:28 -0400

    Box + Receiver working

---
+++ Assets/Scripts/Receiver.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receiver : MonoBehaviour
+{
+    public int recIndex = 1;
+    public bool isActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Box>().boxIndex == recIndex && isActive == false)
+        {
+            isActive = true;
+            Activate();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Box>().boxIndex == recIndex && isActive == true)
+        {
+            isActive = false;
+            Deactivate();
+        }
+    }
+
+    public void Activate()
+    {
+
+    }
+
+    public void Deactivate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Utils/Events.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+
+public static class Events
+{
+    //public static readonly Evt<GameObject> onRoomHover = new Evt<GameObject>(); 
+    //public static readonly Evt onNoRoomHover = new Evt();
+    //public static readonly Evt<Table, TableState> onTableStateChanged = new Evt<Table, TableState>();
+    public static readonly Evt<int> onReceiverActivated = new Evt<int>();
+    public static readonly Evt<int> onReceiverDeactivated = new Evt<int>();
+}
+
+public class Evt
+{
+    private event Action action = delegate { };
+
+    public void Invoke()
+    {
+        action.Invoke();
+    }
+
+    public void AddListener(Action listener)
+    {
+        action += listener;
+    }
+
+    public void RemoveListener(Action listener)
+    {
+        action -= listener;
+    }
+}
+
+public class Evt<T>
+{
+    private event Action<T> action = delegate { };
+
+    public void Invoke(T param)
+    {
+        action.Invoke(param);
+    }
+
+    public void AddListener(Action<T> listener)
+    {
+        action += listener;
+    }
+
+    public void RemoveListener(Action<T> listener)
+    {
+        action -= listener;
+    }
+}
+
+public class Evt<T, T2>
+{
+    private event Action<T, T2> action = delegate { };
+
+    public void Invoke(T param, T2 param2)
+    {
+        action.Invoke(param, param2);
+    }
+
+    public void AddListener(Action<T, T2> listener)
+    {
+        action += listener;
+    }
+
+    public void RemoveListener(Action<T, T2> listener)
+    {
+        action -= listener;
+    }
+}
+
