680e4b5b3663673a5ae31c3894e31265660f1fe6
Grant <grant.macmillan94@gmail.com>
2023-08-04 16:48:09 -0400

    New character movement added, WIP

--- Assets/[Scripts]/NewCharacterController/CharacterMovement.cs
+++ Assets/[Scripts]/NewCharacterController/CharacterMovement.cs
@@ -19,19 +19,35 @@ public class CharacterMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     public CapsuleCollider2D playerBody;
-    public BoxCollider2D playerFeet;
+    public CapsuleCollider2D playerFeet;
 
+    [Header("Movement")]
+    [SerializeField] private float speed = 5;
+    bool facingRight = true;
+
+
+    [Header("Jump")]
+    [SerializeField] private float jumpVelocity;
+    [SerializeField] private float fallMultiplier = 2f, holdJumpMultiplier = 3.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentState = CharacterState.isGrounded;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        Animation();
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 
     private void ChangeState(CharacterState newState)
@@ -49,6 +65,65 @@ public class CharacterMovement : MonoBehaviour
 
     private void Movement()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
 
+        //CHECK FOR FLIPPING CHARACTER
+        if (currentState != CharacterState.isDead)
+        {
+            if (horizontal > 0 && !facingRight)
+            {
+                Flip();
+            }
+
+            if (horizontal < 0 && facingRight)
+            {
+                Flip();
+            }
+        }
+
+        //MOVEMENT AND JUMPING
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        //Changes gravity so you fall faster (makes it feel less floaty when jumping)
+        if (rb.velocity.y < -0.5)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            ChangeState(CharacterState.isFalling);
+        }
+        //if jump is not held more gravity is applied causing a shorter jump (tap jump for less height, hold for more height)
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (holdJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        //jump
+        if (currentState == CharacterState.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = Vector2.up * jumpVelocity;
+        ChangeState(CharacterState.isJumping);
+    }
+
+    private void Flip()
+    {
+        Vector3 currentScale = this.gameObject.transform.localScale;
+        currentScale.x *= -1;
+        this.gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && collision.otherCollider == playerFeet)
+        {
+            //LANDING
+            ChangeState(CharacterState.isGrounded);
+        }
     }
 }
