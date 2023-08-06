3bce48398e514d534dab7b29d86ae72138a58a77
Grant <grant.macmillan94@gmail.com>
2023-08-06 13:29:13 -0400

    New player 1 working with PC

---
+++ Assets/[Scripts]/NewCharacterController/C1_Interact.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C1_Interact : MonoBehaviour
+{
+    public Transform interactDetect;
+    public float rayDistance = 2;
+
+    private void Update()
+    {
+        if (GameManager.Instance.currentCharacter == 1)
+        {
+            InteractRaycast();
+        }
+    }
+
+    public void InteractRaycast()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(interactDetect.position, Vector2.right * transform.localScale, rayDistance);
+        GameObject pc;
+        if (hit.collider != null && hit.collider.tag == "Interactable")
+        {
+            pc = hit.collider.gameObject;
+            if (Input.GetKeyDown(KeyCode.E) && GameManager.Instance.currentCharacter == 1)
+            {
+                Debug.Log("INTERACTED");
+                //HANDLE PC INPUT
+                //Debug.Log("PC ACTIVATED");
+                pc.GetComponent<PC>().Activate();
+            }
+
+        }
+    }
+}
