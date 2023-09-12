6aa46a3589a4c56034fbbaca70174f268397dac4
Grant <grant.macmillan94@gmail.com>
2023-09-12 18:30:17 -0400

    fix for build issue

--- Assets/[Scripts]/NewCharacterController/CharacterMovement.cs
+++ Assets/[Scripts]/NewCharacterController/CharacterMovement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.TextCore.Text;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class CharacterMovement : MonoBehaviour
 {
